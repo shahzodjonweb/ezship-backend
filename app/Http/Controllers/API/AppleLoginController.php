689f395f257d9c6ef98aa36b4532d6f2a10f37d3
shahzodjonweb <shaxaprogrammer@gmail.com>
2023-01-19 19:18:35 -0600

    error fixes

--- app/Http/Controllers/API/AppleLoginController.php
+++ app/Http/Controllers/API/AppleLoginController.php
@@ -4,13 +4,9 @@ namespace App\Http\Controllers\API;
    
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
-use League\OAuth2\Client\Provider\Apple as Apple;
 use App\Models\User;
-use Firebase\JWT\JWT as JWT;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\API\QuickBooksController;
-use FaganChalabizada\AppleTokenAuth\Classes\AppleAuth;
 
 use Validator;
    
@@ -25,19 +21,9 @@ class AppleLoginController extends BaseController
     {
           // check access token
         if ($request->has('code')) {
-       
-        $data = [
-            "client_id" => 'register.ezship.app',
-            "team_id"   => '442HGK87WA',
-            "key_id"    => 'LS6M598Q77',
-            "key"       => __DIR__ .'\AuthKey_LS6M598Q77.p8', //path where is your p8 key example if your key is in storage
-            "code"      => $request->code //code sended by your front end guy
-         ];
-         $appleAuth = new AppleAuth($data);
-         $user = $appleAuth->getUserData();
-         $email = $user['user']->email;
+         $user = Jwt::parseJWT($request->code);;
+         $email = $user->email;
          $name = explode('@', $email)[0];
-         error_log($email .' '. $name);
          $user = User::where('email', $email)->first();
          if ($user) {
             $success['token'] =  $user->createToken('MyApp')-> accessToken; 
