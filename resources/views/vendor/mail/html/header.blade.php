26b7e047c430eef6320f06d42eea86d484ca5e66
shahzodjonweb <shaxaprogrammer@gmail.com>
2023-02-23 14:35:18 -0600

    dasdasd

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <td class="header">
         <a href="https://app.shipio.app/login" style="display: inline-block;">
             @if (trim($slot) === 'EZSHIP')
-                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+                <img src="https://i.ibb.co/djW3vsz/Logo-01-1.png" class="logo" alt="Laravel Logo">
             @else
                 {{ $slot }}
             @endif
